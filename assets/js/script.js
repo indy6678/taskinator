b32a244113869c2ffe79bbb2c45b4bde9046d11c
indy6678 <indy6678@gmail.com>
2022-06-17 14:56:17 -0500

    added li creation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,30 @@
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 var formEl = document.querySelector("#task-form");
 
-var createTaskHandler = function(event) {
+var createTaskHandler = function (event) {
 
   event.preventDefault();
-  
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
+  // console.log(taskTypeInput);
+
+  // create list item
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
+
+  // create div to hold task info and add to list item
+  var taskInfoEl = document.createElement("div");
+
+  // give the div a class name
+  taskInfoEl.className = "task-info";
+
+  // add HTML content to the div
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+  // append div to created li
+  listItemEl.appendChild(taskInfoEl);
+
+  // append li element to list
   tasksToDoEl.appendChild(listItemEl);
 };
 
